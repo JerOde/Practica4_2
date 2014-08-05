4d9cb79f7e259e1bb93a80779b6f03cc1a0706c8
Jerrick Odell Cuahutle Velazquez <odell-07@hotmail.com>
2014-08-05 08:36:57 -0500

    Sonido
    ;

--- js/acciones.js
+++ js/acciones.js
@@ -4,12 +4,12 @@ $(document).ready(function(e){
  
  $('#Beep').click(function(e){
 	 
-navigator.nofification.beep(1)
+navigator.nofification.beep(1);
 	 
 });// click continuar
 $('#Vibrar').click(function(e){
 	 
-navigator.notification.vibrate(1000)
+navigator.notification.vibrate(1000);
 	 
 });// click continuar
 });//ready

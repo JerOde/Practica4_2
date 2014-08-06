db6052e84be7c8e46357708b92517489b33c8331
Jerrick Odell Cuahutle Velazquez <odell-07@hotmail.com>
2014-08-06 08:10:09 -0500

    Sonido
    Better

--- js/acciones.js
+++ js/acciones.js
@@ -1,26 +1,25 @@
 //JavaScript
 	var audio;
 $(document).ready(function(e){	
- 
- if(window.plugins && window.plugins.LowLatencyAudio)
- {audio=window.plugins.LowLatencyAudio;
- audio.preloadFX('mario','audio/mario.mp3',function(msg){},function(msg){alert('error=' + msg);});}//if
- 
- 
+ document.addEventListener("deviceready",function(){
+audio=window.plugins.LowLatencyAudio;
+audio.preloadFX('mario','audio/mario.mp3',function(msg){},function(msg){alert('error=' + msg);});
  
  $('#Beep').bind("tap",function(e){
 	 
 navigator.notification.beep(1);
-	 
-});// click continuar
+});
+
 $('#Vibrar').bind("tap",function(e){
 	 
 navigator.notification.vibrate(1000);
+});
 	 
-});// click continuar
+
 $('#Play').bind("tap",function(e){
 	
 audio.play('mario');
+});
 
-});// click continuar
+});//deviceready
 });//ready
